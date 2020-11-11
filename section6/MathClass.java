06bc93ba808699c355908b6c13e2613bac7793ff
Logan Karnes <logan.karnes98@gmail.com>
2020-11-10 21:15:10 -0800

    Mixed Match

--- section6/MathClass.java
+++ section6/MathClass.java
@@ -1,9 +1,13 @@
 public class MathClass {
     public static void main(String[] args)
     {
-        int number = -100;
+        int number = 121;
         System.out.println(Math.sqrt(number));
         System.out.println(Math.abs(number));
-
+        System.out.println(Math.max(12,number));
+        System.out.println(Math.min(12,number));
+        System.out.println(Math.pow(100, 3));
+        final double pi = 3.14159;
+        System.out.println(Math.round(pi));
     }
 }
---
+++ section6/MixedMatched.java
@@ -0,0 +1,15 @@
+public class MixedMatched {
+    public static void main(String[] args)
+    {
+        // a double can be a int but a int cant be a double
+        double dnum;
+        int num = 2;
+        dnum = num;
+        System.out.println(dnum);
+        //chars will return the ascii value of that char
+        int num2;
+        char c = 'c';
+        num = c; 
+        System.out.println(num);
+    }
+}
